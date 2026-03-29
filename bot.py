9908861d89f1131922fe67b969b3a4bcfce45100
Seripurkon <ryzhovnazar@gmail.com>
2026-03-29 20:53:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,1 @@
-import os
-from telegram.ext import ApplicationBuilder
-
-# ВСТАВЬ СВОЙ ТОКЕН СЮДА В КАВЫЧКИ ДЛЯ ТЕСТА
-TOKEN = "8641381095:AAGLY3W93LQGfq_Ygm1OIfAMwlhb6SlQrXE" 
-
-if __name__ == "__main__":
-    app = ApplicationBuilder().token(TOKEN).build()
-    print("🚀 Бот запустился напрямую!")
-    app.run_polling()
+print("--- ПРОВЕРКА ЗАПУСКА: Я НОВЫЙ КОД ---")
